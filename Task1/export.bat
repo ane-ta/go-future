7abcbd7689e02536c000ccab77233762f33e9691
Anna Voyteshchuk <anna.voyteshchuk@gmail.com>
2026-04-07 16:10:03 +0500

    conf: export org to md

---
+++ Task1/export.bat
@@ -0,0 +1,4 @@
+call ./../export-org.bat ./docs/classification.org ./results/
+call ./../export-org.bat ./docs/requirements.org ./results/
+call ./../export-org.bat ./docs/adr-001-migration.org ./results/
+@REM xcopy /y ".\docs\С2-to-be.puml" ".\results\"
