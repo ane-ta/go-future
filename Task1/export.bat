60ccf8efb688610259d1ad0522ea995f9c0dafc1
Anna Voyteshchuk <anna.voyteshchuk@gmail.com>
2026-05-26 21:40:26 +0500

    conf: arch-tooling added to generate docs

--- Task1/export.bat
+++
@@ -1,7 +0,0 @@
-pushd "%~dp0"
-
-call ./../export-org.bat ./docs/01-requirements.org ./results/
-call ./../export-org.bat ./docs/02-migration.org ./results/
-call ./../export-org.bat ./docs/03-migration-plan.org ./results/
-
-popd
